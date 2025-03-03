7e5efe8e4483160259d517886ffc0aba881ec5fe
Tehilah Zamanzadeh <tehilah.zaman@gmail.com>
2025-03-03 09:39:07 -0800

    9 is not a kwarg - fixed

--- exercises.py
+++ exercises.py
@@ -84,25 +84,10 @@ print('Exercise 8:', product(2, 5, 5))
 
 # Exercise 9: Basic Calculator
 
-# Exercise 9: Basic Calculator
-#
-# Create a function named `basic_calculator` that takes three arguments: 
-# two numbers and a string representing an operation ('add', 'subtract', 'multiply', 'divide'). 
-# Perform the provided operation on the two numbers. In operations where the order of numbers is important, 
-# treat the first parameter as the first operand and the second parameter as the second operand.
-#
-# Examples:
-# basic_calculator(10, 5, 'subtract') should return 5.
-# basic_calculator(10, 5, 'add') should return 15.
-# basic_calculator(10, 5, 'multiply') should return 50.
-# basic_calculator(10, 5, 'divide') should return 2.
-#
-# Define the function and then call it below.
 
+def basic_calculator(num, num2, op):
 
-def basic_calculator(num, num2, kwargs):
-
-# not using kwargs
+#  using if/else
     # if op == "subtract":
     #     sum = num - num2
     # elif op == "add":
@@ -113,14 +98,14 @@ def basic_calculator(num, num2, kwargs):
     #     sum = num / num2
     # return sum 
 
-# using kwargs: 
+#  
     operations = {
         "subtract": num - num2,
         "add":  num + num2,
         "multiply": num * num2,
         "divide": num / num2
     }
-    return operations.get(kwargs)
+    return operations.get(op)
 
 print('Exercise 9 Result:', basic_calculator(10, 5, 'subtract'))
 
